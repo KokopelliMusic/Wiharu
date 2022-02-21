451331b671af328555917b25f8e10f69bc221010
Niels <56843328+Nierot@users.noreply.github.com>
2022-02-21 17:03:31 +0100

    🎨 add env

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -6,6 +6,7 @@ declare global {
             REACT_APP_TAWA_URL: string;
             REACT_APP_SUPABASE_URL: string;
             REACT_APP_SUPABASE_KEY: string;
+            REACT_APP_TOKEN_REFRESH_URL: string;
         }
     }
 
