53a5d9faf5fe30e087ae23999f46dfc88c0770c7
Niels <56843328+Nierot@users.noreply.github.com>
2022-02-22 21:22:55 +0100

    💄 Add custom UI classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",  
+    './src/**/*.{js,jsx,ts,tsx}',  
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '2xl-white': '0 25px 50px 0 rgba(255, 255, 255, 0.25)',
+      },
+      fontFamily: {
+        'player': ['Montserrat', 'ui-sans-serif', 'system-ui']
+      }
+    },
   },
   plugins: [],
 }
