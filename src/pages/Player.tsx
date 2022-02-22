ac5cf543b419ec4c2fff3bf3312c980a42595db5
Niels <56843328+Nierot@users.noreply.github.com>
2022-02-22 21:24:13 +0100

    ✨ Play music !!!!!!

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -4,8 +4,10 @@ import { SessionType } from 'sipapu/dist/src/services/session'
 import { getCode, getSpotify, getUid, SpotifyToken } from '../data'
 import SpotifyWebPlayback from 'react-spotify-web-playback-sdk-headless'
 import FullscreenLoading from './FullscreenLoading'
-import EventHandler from './EventHandler'
 import { PlaylistWithSongsType } from 'sipapu/dist/src/services/playlist'
+import { SongType } from 'sipapu/dist/src/services/song'
+import { ProfileType } from 'sipapu/dist/src/services/profile'
+import Shuffler from './Shuffler'
 
 const LOADING_TIMEOUT = 500
 
@@ -16,7 +18,16 @@ const Player = () => {
   const [playlist, setPlaylist] = useState<PlaylistWithSongsType | undefined>(undefined)
   const [spotify, setSpotify]   = useState<SpotifyToken | undefined>(undefined)
   const [uid, setUid]           = useState<string | undefined>(undefined)
-  const [loading, setLoading]   = useState<boolean>(true)
+  
+  const [loading, setLoading]               = useState<boolean>(true)
+  const [spotifyLoading, setSpotifyLoading] = useState<boolean>(true)
+  const [playerReady, setPlayerReady]       = useState<boolean>(false)
+
+  const [queue, setQueue]     = useState<SongType[]>([])
+  const [event, setEvent]     = useState<SongType | undefined>(undefined)
+  const [addedBy, setAddedBy] = useState<ProfileType | undefined>(undefined)
+  const [paused, setPaused]   = useState<boolean>(false)
+  const [nextSong, setNextSong] = useState<boolean>(false)
   
   useEffect(() => {
     const code = getCode()
@@ -32,6 +43,7 @@ const Player = () => {
       .then(setSpotify)
       .then(() => getUid())
       .then(setUid)
+      .then(() => setTimeout(() => setLoading(false), LOADING_TIMEOUT))
       .catch(err => {
         console.error(err)
         alert('I cannot seem to find your session, sending you back to the home page in 5 seconds')
@@ -42,10 +54,10 @@ const Player = () => {
   useEffect(() => {
     if (!session) return
 
-    window.sipapu.Playlist.getWithSongs(session.playlistId)
-      .then(setPlaylist)
-      .then(() => setTimeout(() => setLoading(false), LOADING_TIMEOUT))
-      .catch(console.error)
+    // window.sipapu.Playlist.getWithSongs(session.playlistId)
+    //   .then(setPlaylist)
+    //   .then(() => setTimeout(() => setLoading(false), LOADING_TIMEOUT))
+    //   .catch(console.error)
 
     const cleanup = window.sipapu.Session.watch(session.id, handleEvent)
 
@@ -55,14 +67,73 @@ const Player = () => {
     }
   }, [session])
 
+  useEffect(() => {
+    console.log(loading, spotifyLoading)
+    if (!loading && !spotifyLoading) {
+      setPlayerReady(true)
+    }
+  }, [loading, spotifyLoading])
+
+  // useEffect(() => {
+  //   if (playerReady !== true) return
+
+  //   const fun = async () => {
+  //     // As soon as everything is done loading we can begin with selecting the next event
+  //     await next()
+  //   }
+
+  //   fun()
+  // }, [playerReady])
+
   /**
-   * TAWA EVENTS
+   * BUSINESS LOGIC
    */
+
+  // const next = async () => {
+  //   let q = queue
+  //   // If queue is empty reshuffle
+  //   if (q.length === 0) {
+  //     console.log('Queue empty, reshuffling')
+  //     q = await updatePlaylist()
+  //   } 
+
+  //   // select the first in the queue
+  //   const nextEvent = q[0]
+  //   // if this event has an 'addedBy' field, then we need to look up the user
+  //   if (nextEvent.addedBy) {
+  //     const user = await window.sipapu.Profile.get(nextEvent.addedBy)
+  //     setAddedBy(user)
+  //   }
+  //   // and save the rest
+  //   console.log('Selected', nextEvent)
+  //   setQueue(q.slice(1))
+  //   setEvent(nextEvent)
+  // }
+
+  // // Fetch the latest playlist
+  // const updatePlaylist = async (): Promise<SongType[]> => {
+  //   console.log('Updating playlist')
+  //   return await window.sipapu.Playlist
+  //     .getWithSongs(session!.playlistId)
+  //     .then(playlist => {
+  //       // Select the new queue 
+  //       const shuffled = shuffleRandomWithoutEvents(playlist)
+  //       const next = shuffled.slice(0, 10)
+    
+  //       console.log(next)
+
+  //       setQueue(next)
+  //       setPlaylist(playlist)
+  //       return next
+  //     })
+  // }
 
   const handleEvent = (event: Event) => {
     switch (event.eventType) {
 
     case EventTypes.PLAY_PAUSE:
+      // TODO: handle for all players
+      player.current?.togglePlay()
       console.log('play/pause')
       break
       
@@ -83,8 +154,12 @@ const Player = () => {
       .catch(console.error)
   }
 
-  const onSongFinished = () => {
-    alert('Song finished')
+  const onSpotifyReady = () => {
+    setSpotifyLoading(false)
+  }
+
+  const onSongFinished = async () => {
+    setNextSong(p => !p)
   }
 
   if (loading) return <FullscreenLoading />
@@ -93,7 +168,7 @@ const Player = () => {
     <SpotifyWebPlayback
       ref={player}
       name="Kokopelli"
-      volume={100}
+      volume={window.location.href.includes('localhost') ? 2 : 100}
       debug
       logging
 
@@ -103,13 +178,25 @@ const Player = () => {
       refreshTokenUrl={process.env.REACT_APP_TOKEN_REFRESH_URL}
 
       onTokenRefresh={onTokenRefresh}
+      onReady={onSpotifyReady}
       songFinished={onSongFinished}
     />
-    <EventHandler
-      session={session!}
-      playlist={playlist!}
+
+    {spotifyLoading ? null : <Shuffler
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      session={session!}    
+      paused={paused}
+      songFinished={nextSong}
+      spotifyPlayer={player} />}
+
+    {/* {spotifyLoading ? null : <EventHandler
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      playlist={playlist!} session={session!}
       spotifyPlayer={player}
-    />
+      event={event}
+      user={addedBy}
+      paused={paused}
+    />} */}
   </div>
 }
 
